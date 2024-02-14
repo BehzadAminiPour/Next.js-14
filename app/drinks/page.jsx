446fc63511027b5cf09da4781335084303578c7c
behzad <behzadaminipour87@gmail.com>
2024-02-14 13:13:27 +0330

    Add error page

--- app/drinks/page.jsx
+++ app/drinks/page.jsx
@@ -2,6 +2,7 @@ const DrinksPage = async () => {
   const response = await fetch(
     "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
   );
+
   const drinks = await response.json();
 
   return <div>Drinks</div>;
