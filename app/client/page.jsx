b082501cd625e445fea570ef0da74ed365b88e6b
behzad <behzadaminipour87@gmail.com>
2024-02-14 12:33:54 +0330

    Using use client in client page

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -1,5 +1,25 @@
+"use client";
+import { useState } from "react";
+
 const ClientPage = () => {
-  return <div>ClientPage</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div className="flex gap-4 justify-center items-center">
+      <button
+        onClick={() => setCount(count - 1)}
+        className="btn btn-md bg-blue-500 text-slate-50 hover:bg-blue-400"
+      >
+        -
+      </button>
+      <p className="text-4xl">{count}</p>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="btn btn-md bg-red-500 text-slate-50 hover:bg-red-400"
+      >
+        +
+      </button>
+    </div>
+  );
 };
 
 export default ClientPage;
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-blue-700">Hello.I am back again.</h1>
-      <button className="btn btn-primary btn-xs">Click Me</button>
+      <h1 className="text-blue-700 text-4xl mb-4">Hello.I am back again.</h1>
+      <Link href="/client" className="btn btn-primary btn-sm">
+        Get Started
+      </Link>
     </>
   );
 }
