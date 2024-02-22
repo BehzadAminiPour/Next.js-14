4f466c2a232b6c030a4ef1f4258a539fe9d5c1a9
behzad <behzadaminipour87@gmail.com>
2024-02-22 12:42:53 +0330

    Link to task id

--- app/components/DeleteForm.jsx
+++ app/components/DeleteForm.jsx
@@ -1,5 +1,9 @@
 const DeleteForm = () => {
-  return <button className="btn btn-sm bg-red-400 ">Delete</button>;
+  return (
+    <button className="btn btn-sm bg-red-400 ml-1 hover:bg-red-500">
+      Delete
+    </button>
+  );
 };
 
 export default DeleteForm;
--- app/components/TaskList.jsx
+++ app/components/TaskList.jsx
@@ -11,12 +11,15 @@ const TaskList = async () => {
   if (tasks.length === 0) return <h1>There is no task to show!</h1>;
 
   return (
-    <ul>
+    <ul className="mt-8">
       {tasks.map((task) => (
-        <li key={task.id}>
-          <h2>{task.content}</h2>
+        <li
+          key={task.id}
+          className="flex justify-between items-center mb-4 px-6 py-4  border border-base-300 rounded-lg shadow-lg"
+        >
+          <h2 className="">{task.content}</h2>
           <div>
-            <Link href="/" className="btn btn-accent btn-sm">
+            <Link href={`/tasks/${task.id}`} className="btn btn-accent btn-sm">
               Edit
             </Link>
             <DeleteForm />
