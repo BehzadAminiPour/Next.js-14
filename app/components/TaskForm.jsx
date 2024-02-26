96603f363f54575353a241e12d23ebf3e67c6ba3
behzad <behzadaminipour87@gmail.com>
2024-02-26 23:38:11 +0330

    changing taskForm to client component

--- app/components/TaskForm.jsx
+++ app/components/TaskForm.jsx
@@ -1,6 +1,12 @@
+"use client";
 const TaskForm = () => {
   return (
-    <form className="mb-8">
+    <form
+      className="mb-8"
+      onClick={(e) => {
+        e.preventDefault();
+      }}
+    >
       <input
         type="text"
         className=" p-1 rounded-md border border-base-300 shadow-sm"
